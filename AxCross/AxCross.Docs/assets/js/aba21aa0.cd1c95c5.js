3fe91f14f791d75e6677837ede03531740b4f137
Santiago <santiago@axiontecnologia.com.br>
2026-08-07 19:47:57 -0300

    fix(git): remover artefatos de build AxCross.Docs do tracking e atualizar .gitignore

--- AxCross/AxCross.Docs/assets/js/aba21aa0.cd1c95c5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkaxion_axcross_docs_portal=globalThis.webpackChunkaxion_axcross_docs_portal||[]).push([[742],{7093(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
